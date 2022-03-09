8534981226393963f4ebe7da8d314bf8d8ac93d0
Pranav Vohra <pranavvohra2000@gmail.com>
2022-03-09 15:06:09 +0530

    Untested API's

--- server/src/routes/auth/index.js
+++ server/src/routes/auth/index.js
@@ -4,6 +4,6 @@ const router= express.Router();
 
 router.use('/student', require('./student'));
 router.use('/agency', require('./agency'));
-router.use('/university', require('./university'));
+router.use('/institute', require('./institute'));
 
 module.exports= router;
